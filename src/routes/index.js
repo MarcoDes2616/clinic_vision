dce5127dcd697f9278f74254a10e1a021c33387f
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-10-06 22:41:30 -0500

    system.routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const userRouter = require('./users.routes');
+const systemRouter = require('./system.routes');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use("/users", userRouter)
 
+router.use("/system", systemRouter)
 
 module.exports = router;
