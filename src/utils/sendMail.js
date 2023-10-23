ff3ab3d9804221e526d6df62a155e4bee3064c5e
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-10-23 00:12:46 -0500

    Update sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,10 +1,5 @@
 const nodemailer = require("nodemailer");
-const { Resend } = require('resend')
 require("dotenv").config(); 
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-module.exports = resend;
 
 const sendEmail = (options) => new Promise((resolve, reject) => {
 
