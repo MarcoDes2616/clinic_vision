e9c55837a3475d80ff6cae13594d749b32682926
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-01-15 09:51:47 -0400

    Update system.controllers.js

--- src/controllers/system.controllers.js
+++ src/controllers/system.controllers.js
@@ -22,7 +22,6 @@ const login = catchError(async (req, res) => {
 const resetPaswwordMail = catchError(async (req, res) => {
   const { email } = req.body;
   const user = await Users.findOne({ where: { email } });
-  console.log(user);
   if (!user || !user?.status) return res.status(404).json({ message: "User no found" })
   const tokenToVerify = jwt.sign({ user }, process.env.TOKEN_SECRET, {
     expiresIn: "1h"});
