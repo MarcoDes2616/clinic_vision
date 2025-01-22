069ced281aafd1f3a646f8da60ca1cbc8463a6d2
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-01-22 15:34:38 -0400

    update init file

--- src/models/DiagnosisList.js
+++ src/models/DiagnosisList.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
 
-const DiagnosisList = sequelize.define('diagnosis_list', {
+const DiagnosisList = sequelize.define('diagnosisList', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,7 @@ const Attention = require("./Attention");
 const Location = require("./Location");
 const Measurement = require("./Measurement");
 const RxUse = require("./RxUse");
+const DiagnosisList = require("./DiagnosisList");
 
 const initModels = () => {
 
@@ -15,38 +16,30 @@ const initModels = () => {
 
   // enlistment 1 ----- * patients
   Enlistment.hasMany(Patient);
-  // Patient.belongsTo(Enlistment, { foreignKey: "enlistmentId" });
   
   // patients 1 ----- 1 clinicHistory
   Patient.hasOne(ClinicHistory);
-  // ClinicHistory.belongsTo(Patient, { foreignKey: "patientId" });
   
   // measurements 1 ----- 1 attention
   Attention.hasOne(Measurement);
-  // Measurement.belongsTo(Attention, { foreignKey: "attentionId" });
   
   // prescription 1 ----- 1 attention
   Attention.hasOne(RxUse);
-  // RxUse.belongsTo(Attention, { foreignKey: "attentionId" });
   
   // locations 1 ----- * attention
   Location.hasMany(Attention);
-  // Attention.belongsTo(Location, { foreignKey: "locationId" });
 
   // enlistment 1 ----- * locations
   Enlistment.hasMany(Location);
-  // Location.belongsTo(Enlistment, { foreignKey: "enlistmentId" });
   
   // clinicHistory 1 ----- * attention
   ClinicHistory.hasMany(Attention);
-  // Attention.belongsTo(ClinicHistory, { foreignKey: "clinicHistoryId" });
   
   // users 1 ----- * attention
   Users.hasMany(Attention);
-  // Attention.belongsTo(Users, { foreignKey: "userId" });
 
-  // Attention.hasMany("diagnosis_atention", {foreignKey: "attentionId"})
-
+  // attention * ----- * diagnosisList
+  Attention.belongsToMany(DiagnosisList, { through: 'diagnosis_attention' })
 };
 
 module.exports = initModels;
