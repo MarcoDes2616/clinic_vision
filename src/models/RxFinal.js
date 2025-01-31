5e0e2b25a73d1f3e4af7ddc97232d61cfea33784
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-01-31 11:02:39 -0400

    Update RxFinal.js

--- src/models/RxFinal.js
+++ src/models/RxFinal.js
@@ -2,27 +2,27 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
 
 const RxFinal = sequelize.define('rx_finals', {
-    od_used: {
+    od_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     },
-    od_add_used: {
+    od_add_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     },
-    oi_used: {
+    oi_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     },
-    oi_add_used: {
+    oi_add_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     },
-    od_axis_used: {
+    od_axis_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     },
-    oi_axis_used: {
+    oi_axis_final: {
         type: DataTypes.STRING(20),
         allowNull: true
     }
